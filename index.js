972394912e078a6a7fa56ee2c93f08265f90773e
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2023-01-18 11:46:12 -0300

    Ler e Escrever no arquivo JSON

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ fs.writeFile("./teste.txt", "Nome: Abiezer José Junior", (err) => {
     console.log("Falha ao adicionar texto no Arquivo");
   }
 });
+
+function modificarArquivo(nome, curso, categoria) {
+  fs.readFile("./usuario.json", { encoding: "utf-8" }, (err, dados) => {
+    if (err) {
+      console.log("Erro na leitura de arquivo!");
+    } else {
+      // Tranformar texto para Objeto JAVASCRIPT
+      var conteudo = JSON.parse(dados);
+
+      //Alterando o conteúdo inicial do arquivo usuuario.json
+      conteudo.nome = nome;
+      conteudo.curso = curso;
+      conteudo.categoria = categoria;
+
+      //Tranformar objeto javascritp para tetxo com Json.stringfy
+      fs.writeFile("./usuario.json", JSON.stringify(conteudo), (err) => {
+        if (err) {
+          console.log("Falha ao escrever o arquivo!");
+        } else {
+          console.log(conteudo);
+        }
+      });
+    }
+  });
+}
+
+modificarArquivo("Abiezer Odanaca", "React", "Web");
